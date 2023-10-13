074fef90d022a6b9b38dd78a73b14e0dbf326aed
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-13 20:24:53 +0300

    feat: mydeepface enroll model

--- mydeepface/models.py
+++ mydeepface/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Enroll(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    user_id = models.UUIDField()
+    image = models.ImageField(upload_to="mydeepface/uploads/")
