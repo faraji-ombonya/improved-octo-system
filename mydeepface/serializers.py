1e2588ea252d8d8cc4b1983ffedf50c5c192bd3c
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-13 20:25:10 +0300

    feat: deepface enroll serializer

---
+++ mydeepface/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Enroll
+
+class EnrollSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enroll
+        fields = '__all__'
+    
