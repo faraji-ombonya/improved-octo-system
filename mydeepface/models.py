91d3c221a818bf3de42fe27233707ee78f744b9f
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-13 23:33:00 +0300

    feat: recognize model

--- mydeepface/models.py
+++ mydeepface/models.py
@@ -5,3 +5,7 @@ class Enroll(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     user_id = models.UUIDField()
     image = models.ImageField(upload_to="mydeepface/uploads/")
+
+class Recognize(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    image = models.ImageField(upload_to="mydeepface/uploads/")
