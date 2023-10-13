e28d4ea5b60586a6ad4d7db53697b1374d42125a
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-13 23:34:22 +0300

    update: filemanagement utils

--- mydeepface/utils/file_management.py
+++ mydeepface/utils/file_management.py
@@ -2,20 +2,6 @@ import os
 import shutil
 import re
 
-def create_user_database_folder(name):
-    # Define the base directory where you want to create the folder
-    base_directory = "user/database"
-
-    # Combine the base directory and the name to create the full path
-    folder_path = os.path.join(base_directory, name)
-
-    try:
-        # Create the folder if it doesn't exist
-        os.makedirs(folder_path)
-        print(f"Folder '{folder_path}' created successfully.")
-    except OSError as e:
-        print(f"Error creating folder '{folder_path}': {str(e)}")
-
 
 def move_image(source_path, destination_path):
     try:
@@ -43,19 +29,20 @@ def delete_file(file_path):
         print(f"Error deleting file '{file_path}': {str(e)}")
 
 
-# get user id from image path
+
 def get_user_id_from_image_path(image_path):
-    # Define a regular expression pattern to match the userID (assuming it's a numeric value)
-    pattern = r'/(\d+)/'
+    # Define a regular expression pattern to match the user ID (assuming it's a UUID format)
+    pattern = r'/database/([0-9a-f-]+)/'
 
     # Use re.search to find the match
     match = re.search(pattern, image_path)
 
     # Check if a match was found
     if match:
-        # Extract the userID from the match
+        # Extract the user ID from the match
         user_id = match.group(1)
         return user_id
     else:
         return None
+
 
