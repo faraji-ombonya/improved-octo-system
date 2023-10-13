8699d001057959e24805aa0d1f05fc7bd2918161
Faraji Ombonya <farajiombonya@gmail.com>
2023-10-13 23:33:40 +0300

    feat: recognize serializer

--- mydeepface/serializers.py
+++ mydeepface/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Enroll
+from .models import Enroll, Recognize
 
 class EnrollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Enroll
         fields = '__all__'
     
+class RecognizeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recognize
+        fields = '__all__'
+        
